284c6515a66042af3b4af7c6fbf31d66a1e896c2
Eduardo <emaiarezende@gmail.com>
2023-04-29 18:37:09 -0300

    Co-authored-by: Klyssmann Oliveira <56873266+klyssmannoliveira@users.noreply.github.com>

---
+++ src/Hero.hs
@@ -0,0 +1,23 @@
+-- Módulo Hero
+module Hero where
+
+-- | Heróis presentes no jogo (Heróis exemplo)
+data Hero = Batman | Superman | MulherMaravilha | Flash | Aquaman | LanternaVerde | CacadorDeMarte | MulherGaviao | Cyborg | CanarioNegro
+  deriving (Show, Eq)
+
+data Characteristic = Voa | SuperForça | Arma | Teleporte | Rapido | FalaComAnimais | RespiraDebaixoDagua | SuperSentidos | FatorDeCura | SuperInteligencia
+  deriving (Show, Eq)
+
+
+-- | Um dicionário que associa cada herói com um conjunto de características
+heroCharacteristics :: Hero -> [Characteristic]
+heroCharacteristics Batman = [Arma]
+heroCharacteristics Superman = [Voa, SuperForça, SuperSentidos, FatorDeCura]
+heroCharacteristics MulherMaravilha = [Voa, SuperForça, Arma, SuperSentidos]
+heroCharacteristics Flash = [Rapido]
+heroCharacteristics Aquaman = [FalaComAnimais, RespiraDebaixoDagua, SuperForça]
+heroCharacteristics LanternaVerde = [Arma]
+heroCharacteristics CacadorDeMarte = [Teleporte, SuperForça, SuperSentidos]
+heroCharacteristics MulherGaviao = [Voa, Arma, SuperForça]
+heroCharacteristics Cyborg = [Arma, SuperForça]
+heroCharacteristics CanarioNegro = [Arma, SuperSentidos]
