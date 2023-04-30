86064749e21e72df517ab448c6d74412ee8898a3
klyssmannoliveira <klyssmannoliveira@gmail.com>
2023-04-30 14:51:46 -0300

    Arvore da DC completa
    Co-authored-by: biancasofia <sofiabrasil44@gmail.com>
    Co-authored-by: adrian soares <adriansoareslps@gmail.com>

--- src/Hero.hs
+++ src/Hero.hs
@@ -2,7 +2,7 @@
 module Hero where
 
 -- | Heróis presentes no jogo (Heróis exemplo)
-data Hero = Batman | Superman | MulherMaravilha | Flash | Aquaman | LanternaVerde | CacadorDeMarte | MulherGaviao | Cyborg | CanarioNegro | Batgirl | Zattana | Ciborgue
+data Hero = Batman | Superman | MulherMaravilha | Flash | Aquaman | LanternaVerde | CacadorDeMarte | MulherGaviao | Cyborg | CanarioNegro | Batgirl | Zattana | Ciborgue | SuperChoque | DrDestino | Nuclear | HomemElastico | Demolidor | ArqueiroVerde | Pacificador
   deriving (Show, Eq)
 
 data Characteristic = Voa | SuperForça | Arma | Teleporte | Rapido | FalaComAnimais | RespiraDebaixoDagua | SuperSentidos | FatorDeCura | SuperInteligencia
--- src/Tree.hs
+++ src/Tree.hs
@@ -10,36 +10,56 @@ data Tree a = Leaf a | Node String (Tree a) (Tree a)
 tree :: Tree (Either String Hero)
 tree =
   Node "O herói que você está pensando é da DC?"
-  (Node "O herói é do gênero feminino?"
-    (Node "O herói é da raça humana?"
-      (Node "O herói é filha do comissário de Gotham?"
-        (Leaf (Right Batgirl))
-        (Leaf (Right CanarioNegro))
+    (Node "O herói é do gênero feminino?"
+      (Node "O herói é da raça humana?"
+        (Node "O herói é filha do comissário de Gotham?"
+          (Leaf (Right Batgirl))
+          (Leaf (Right CanarioNegro))
+        )
+        (Node "O herói é um Deus ou semi-Deus?"
+          (Leaf (Right MulherMaravilha))
+          (Leaf (Right Zattana))
+        )
       )
-      (Node "O herói é um Deus ou semi-Deus?"
-        (Leaf (Right MulherMaravilha))
-        (Leaf (Right Zattana))
-      )
-    )
-    (Node "O herói é um Deus, semi-Deus ou extraterrestre?"
-      (Node "O herói é de Krypton?"
-        (Leaf (Right Superman))
-        (Leaf (Right CacadorDeMarte))
-      )
-      (Node "O herói é uma IA?"
-        (Leaf (Right Ciborgue))
-        (Node "O herói é um mutante?"
-          (Node "O herói usa um anel de poder?"
-            (Leaf (Right LanternaVerde))
-            -- continuar a árvore aqui
-            (Leaf (Left "Não sei"))
+      (Node "O herói é um Deus, semi-Deus ou extraterrestre?"
+        (Node "O herói é de Krypton?"
+          (Leaf (Right Superman))
+          (Leaf (Right CacadorDeMarte))
+        )
+        (Node "O herói é uma IA?"
+          (Leaf (Right Ciborgue))
+          (Node "O herói é um mutante?"
+            (Node "O herói usa um anel de poder?"
+              (Leaf (Right LanternaVerde))
+              (Node "O herói possui super velocidade?"
+                (Leaf (Right Flash))
+                (Node "O herói usa habilidades elétricas?"
+                  (Leaf (Right SuperChoque))
+                  (Node "O herói tem poderes telepáticos?"
+                    (Leaf (Right DrDestino))
+                    (Node "O herói possui habilidades nucleares?"
+                      (Leaf (Right Nuclear))
+                      (Leaf (Right HomemElastico))
+                    )
+                  )
+                )
+              )
+            )
+            -- continuar a arvore aqui
+            (Node "O herói é um lutador cego?"
+              (Leaf (Right Demolidor))
+              (Node "O herói é um arqueiro?"
+                (Leaf (Right ArqueiroVerde))
+                (Node "O herói é um mestre em combate corpo a corpo?"
+                  (Leaf (Right Batman))
+                  (Leaf (Right Pacificador))
+                )
+              )
+            )
           )
-          -- continuar a arvore aqui
-          (Leaf (Left "Não sei"))
         )
       )
     )
-  )
   (Leaf (Left "Desculpe, não consegui identificar o herói que você está pensando"))
 
 
@@ -66,7 +86,7 @@ runGame (Leaf (Left message)) = do
   putStrLn message
   runGame tree
 runGame (Leaf (Right hero)) = do
-  putStrLn ("O herói em que você está pensando é " ++ show hero ++ ".")
+  -- | putStrLn ("O herói em que você está pensando é " ++ show hero ++ ".")
   return hero
 runGame (Node question left right) = do
   putStrLn question
