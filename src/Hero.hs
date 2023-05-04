c718c886310482847f36a7cae58aa40fc462de2a
ErickMVdO <erickmelovidal@gmail.com>
2023-05-03 22:43:18 -0300

    corrigindo nomenclatura dos herois
    Co-authored-by: Daniel <danielribeiroalves0@gmail.com>

--- src/Hero.hs
+++ src/Hero.hs
@@ -2,22 +2,22 @@
 module Hero where
 
 -- | Heróis presentes no jogo (Heróis exemplo)
-data Hero = Batman | Superman | MulherMaravilha | Flash | Aquaman | LanternaVerde | CacadorDeMarte | MulherGaviao | CanarioNegro | Batgirl | Zattana | Ciborgue | SuperChoque | DrDestino | Nuclear | HomemElastico | DrMeiaNoite | ArqueiroVerde | Pacificador | Visao | Hercules | Thor | CapitaoAmerica | HomemDeFerro | Wolverine | Demolidor | Blade | GaviaoArqueiro | Falcao | Justiceiro | Groot | Quasar | RocketRaccoon | ViuvaNegra | Vespa | Gamora | Tempestade | JeanGrey | JessicaJones | Cyborg | InvalidAnswer
+data Hero = Batman | Superman | MulherMaravilha | Flash | Aquaman | LanternaVerde | CacadorDeMarte | MulherGaviao | CanarioNegro | Batgirl | Zattana | Ciborgue | SuperChoque | SrDestino | Nuclear | HomemElastico | DrMeiaNoite | ArqueiroVerde | Pacificador | Visao | Hercules | Thor | CapitaoAmerica | HomemDeFerro | Wolverine | Demolidor | Blade | GaviaoArqueiro | Falcao | Justiceiro | Groot | Quasar | RocketRaccoon | ViuvaNegra | Vespa | Gamora | Tempestade | JeanGrey | JessicaJones | Cyborg | InvalidAnswer
   deriving (Show, Eq)
 
-data Characteristic = Voa | SuperForça | Arma | Teleporte | Rapido | FalaComAnimais | RespiraDebaixoDagua | SuperSentidos | FatorDeCura | SuperInteligencia
-  deriving (Show, Eq)
+-- data Characteristic = Voa | SuperForça | Arma | Teleporte | Rapido | FalaComAnimais | RespiraDebaixoDagua | SuperSentidos | FatorDeCura | SuperInteligencia
+--   deriving (Show, Eq)
 
 
 -- | Um dicionário que associa cada herói com um conjunto de características
-heroCharacteristics :: Hero -> [Characteristic]
-heroCharacteristics Batman = [Arma]
-heroCharacteristics Superman = [Voa, SuperForça, SuperSentidos, FatorDeCura]
-heroCharacteristics MulherMaravilha = [Voa, SuperForça, Arma, SuperSentidos]
-heroCharacteristics Flash = [Rapido]
-heroCharacteristics Aquaman = [FalaComAnimais, RespiraDebaixoDagua, SuperForça]
-heroCharacteristics LanternaVerde = [Arma]
-heroCharacteristics CacadorDeMarte = [Teleporte, SuperForça, SuperSentidos]
-heroCharacteristics MulherGaviao = [Voa, Arma, SuperForça]
-heroCharacteristics Cyborg = [Arma, SuperForça]
-heroCharacteristics CanarioNegro = [Arma, SuperSentidos]
+-- heroCharacteristics :: Hero -> [Characteristic]
+-- heroCharacteristics Batman = [Arma]
+-- heroCharacteristics Superman = [Voa, SuperForça, SuperSentidos, FatorDeCura]
+-- heroCharacteristics MulherMaravilha = [Voa, SuperForça, Arma, SuperSentidos]
+-- heroCharacteristics Flash = [Rapido]
+-- heroCharacteristics Aquaman = [FalaComAnimais, RespiraDebaixoDagua, SuperForça]
+-- heroCharacteristics LanternaVerde = [Arma]
+-- heroCharacteristics CacadorDeMarte = [Teleporte, SuperForça, SuperSentidos]
+-- heroCharacteristics MulherGaviao = [Voa, Arma, SuperForça]
+-- heroCharacteristics Cyborg = [Arma, SuperForça]
+-- heroCharacteristics CanarioNegro = [Arma, SuperSentidos]
