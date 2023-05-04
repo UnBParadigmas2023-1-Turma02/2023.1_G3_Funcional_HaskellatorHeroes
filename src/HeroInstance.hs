c605eff40e6f97ee999bdb97c05bd46c51408d0d
adrian_s_lps <adriansoareslps@gmail.com>
2023-05-04 12:16:46 -0300

    Creates structure to search for Hero
    Co-authored-by:  joaobisi <jbisinotti@gmail.com>

--- src/HeroInstance.hs
+++ src/HeroInstance.hs
@@ -2,13 +2,40 @@ module HeroInstance where
 
 import Hero
 import System.FilePath (pathSeparator)
+import qualified Data.Map as M
 
 data HeroInstance = HeroInstance {hero :: Hero, name :: String, imgPath :: String} 
   deriving (Show)
 
-baseImgPath = "img" ++ [pathSeparator]
+basePath = ".." ++ [pathSeparator] ++ "img" ++ [pathSeparator]
+bmpFolder = "." ++ [pathSeparator] ++ "bmp" ++ [pathSeparator]
 
-mulherMaravilha :: HeroInstance
-mulherMaravilha = HeroInstance{hero = MulherMaravilha, name = "Mulher Maravilha", imgPath = baseImgPath ++ "mulherMaravilha.png"}
+heroesData :: M.Map Hero (String, String)
+heroesData = M.fromList $ map (\x -> (hero x, (name x, imgPath x))) heroList
+  where
+    mm :: HeroInstance 
+    mm = HeroInstance{hero = MulherMaravilha, name = "Mulher Maravilha", imgPath = bmpFolder ++ "mulherMaravilha.bmp"}
 
-{--heroes :: [HeroIn] --}
+    bld :: HeroInstance
+    bld = HeroInstance Blade "Blade" "Blade.bmp"
+
+    --Continua
+
+
+    heroList = [bld, mm]
+
+getHeroName :: Hero -> String
+getHeroName hero = case M.lookup hero heroesData of
+  Just (x, _) -> x
+  Nothing -> ""
+
+getHeroImg :: Hero -> String
+getHeroImg hero = case M.lookup hero heroesData of 
+  Just (_, y) -> y
+  Nothing -> ""
+
+testMap = print imgPath
+  where 
+    (_, imgPath) = heroesData M.! MulherMaravilha
+  
+
